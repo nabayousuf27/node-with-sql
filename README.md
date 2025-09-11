06aaee910cdddaa99336ed771d54815ebf03c215
Naba Yousuf <nabay0519@gmail.com>
2025-09-11 14:30:00 +0500

    edit route

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Connecting Databases with Backened
+### Open MySQL in Terminal
+
+To open MySQL in the terminal, run this command:
+
+```cmd
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p
--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const { faker } = require('@faker-js/faker');
 const express =  require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname, "/views"));
+//to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 
 // Create the connection to database
@@ -28,7 +32,7 @@ let  getRandomUser =  () =>  {
   return[ 
     faker.string.uuid(),
     faker.internet.username(),
-    faker.internet.email(),
+    faker.internet.email(), 
     faker.internet.password(),
   ];
 }
@@ -95,7 +99,7 @@ connection.query(q, (err,result) => {
 // })
 //show user 
 app.get("/user", (req,res) => {
-  let q = 'select username from user';
+  let q = 'select * from user';
   try{
   connection.query(q, (err,result) => {
   if(err) throw err;
@@ -108,7 +112,43 @@ app.get("/user", (req,res) => {
   res.send("some error in database");
 }
 } )
+//edit route
+app.get("/user/:id/edit" , (req,res) => {
+  let { id } = req.params;
+  // let {username} = req.body;
+  // let {password} = req.body;
+  //for databases
+      let q = `select * from user where id = '${id}'`;
 
+      try{
+      connection.query(q, (err,result) => {
+      if(err) throw err;
+      let userforedit = result[0];  //it has id ,username, password, and emails object
+      res.render("edit.ejs",{userforedit});
+      });
+
+    }catch(err){
+      console.log(err);
+      res.send("some error in database");
+    }
+  //  //logic
+  //  if(password == userforedit.password){
+  //   userforedit.password = password;
+  //  }else{
+  //   //password remains same
+  //  }
+
+  // console.log(req.body)
+  // console.log(id)
+  // const {username} = req.body;
+  // console.log(username)
+  // res.send("hey you are in edit route")
+});
+
+//update route
+app.patch("/user/:id" , (req,res)=>{
+  res.send("req working")
+})
 app.listen ("8080" , () => {
   console.log("server is listening to port 8080");
 })
